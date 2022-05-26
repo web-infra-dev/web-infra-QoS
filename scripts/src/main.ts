550fd1eb9c94063f2afd5222600a7953e5c63a9d
chenjiahan <chenjiahan.jait@bytedance.com>
2022-05-26 23:49:18 +0800

    fix: lock @types/react version

--- scripts/src/main.ts
+++ scripts/src/main.ts
@@ -37,8 +37,22 @@ async function cloneModernJs() {
     content => `${content}\n - 'cases/*'`,
   );
 
+  // lock @types/react version
+  await updateFile(join(MODERN_PATH, 'package.json'), content => {
+    const json = JSON.parse(content);
+    json.pnpm = {
+      ...json.pnpm?.overrides,
+      '@types/react': '^17',
+      '@types/react-dom': '^17',
+    };
+    return JSON.stringify(json, null, 2);
+  });
+
   await runCommand(MODERN_PATH, 'pnpm link ../scripts');
-  await runCommand(MODERN_PATH, 'pnpm install --ignore-scripts --no-frozen-lockfile');
+  await runCommand(
+    MODERN_PATH,
+    'pnpm install --ignore-scripts --no-frozen-lockfile',
+  );
   await runCommand(MODERN_PATH, 'pnpm prepare');
 }
 
