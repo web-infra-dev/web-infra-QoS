d5f637bb86ff6da3819643d10cfd140791434b11
chenjiahan <chenjiahan.jait@bytedance.com>
2023-09-21 16:59:05 +0800

    chore: support new data format

--- website/src/shared/request.ts
+++ website/src/shared/request.ts
@@ -1,6 +1,8 @@
 import { COMMITS_INFO_URL, PUBLIC_PATH } from './constant';
 import type { Metrics } from '../../../scripts/src/shared/types';
 
+export type MetricsMap = Record<string, Metrics & { time: number }>;
+
 export type CommitInfo = {
   id: string;
   time: number;
@@ -10,42 +12,10 @@ export type FetchedMetrics = CommitInfo & {
   metrics: Record<string, any>;
 };
 
-const map = new Map();
-
-const cache = {
-  get(key: string) {
-    if (map.has(key)) {
-      return map.get(key);
-    }
-    const data = localStorage.getItem(key);
-    if (data) {
-      try {
-        map.set(key, JSON.parse(data));
-      } catch (err) {
-        return undefined;
-      }
-    }
-    return map.get(key);
-  },
-  set(key: string, data: Record<string, unknown>) {
-    if (data) {
-      map.set(key, data);
-      try {
-        localStorage.setItem(key, JSON.stringify(data));
-      } catch (err) {}
-    }
-  },
-};
-
 const fetchJsonWithCache = async (url: string) => {
-  const cachedValue = cache.get(url);
-  if (cachedValue) {
-    return cachedValue;
-  }
   const data = await fetch(url)
     .then(res => res.json())
     .catch(() => {});
-  cache.set(url, data);
   return data;
 };
 
@@ -55,18 +25,15 @@ export const fetchCommitsInfo = async () =>
 export const fetchMetrics = async (
   caseName: string,
 ): Promise<FetchedMetrics[]> => {
-  const commitsInfo: CommitInfo[] = await fetchCommitsInfo();
-
-  const allMetrics: Metrics[] = await Promise.all(
-    commitsInfo.map(info =>
-      fetchJsonWithCache(PUBLIC_PATH + `/data/${info.id}/${caseName}.json`),
-    ),
+  const allMetrics: MetricsMap = await fetchJsonWithCache(
+    PUBLIC_PATH + `/data/${caseName}.json`,
   );
 
-  return allMetrics
-    .map((metrics, index) => ({
-      ...commitsInfo[index],
-      metrics,
-    }))
-    .filter(item => !!item.metrics);
+  return Object.keys(allMetrics).map(id => {
+    return {
+      id,
+      time: allMetrics[id].time,
+      metrics: allMetrics[id],
+    };
+  });
 };
