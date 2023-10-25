df4cceb96cec829cb757ddb984378fb82702f520
Timeless0911 <50201324+Timeless0911@users.noreply.github.com>
2023-10-25 18:38:01 +0800

    fix: regroup path (#24)

--- scripts/src/shared/constant.ts
+++ scripts/src/shared/constant.ts
@@ -3,9 +3,9 @@ import { join } from 'path';
 export const DEFAULT_RERUN_TIME = 5;
 export const ROOT_PATH = join(__dirname, '..', '..', '..');
 export const MODERN_PATH = join(ROOT_PATH, 'modern.js');
-export const TEMP_PATH = join(ROOT_PATH, 'temp');
+export const TEMP_PATH = join(ROOT_PATH, 'temp', 'modernjs-framework');
 export const DATA_PATH = join(ROOT_PATH, 'data', 'modernjs-framework');
 export const CASES_SRC_PATH = join(ROOT_PATH, 'cases', 'modernjs-framework');
 export const CASES_DIST_PATH = join(MODERN_PATH, 'cases');
 export const REMOTE_DATA_URL =
-  'https://github.com/web-infra-dev/web-infra-QoS/raw/gh-pages/data';
+  'https://github.com/web-infra-dev/web-infra-QoS/raw/gh-pages/data/modernjs-framework';
