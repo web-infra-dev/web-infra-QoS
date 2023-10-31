a18f88bf2d2f0eb273aa06b1a127f13e46b57a4c
戴陈威 <daichenwei@bytedance.com>
2023-10-31 21:49:26 +0800

    chore: json cache 12 hours

--- website/src/shared/request.ts
+++ website/src/shared/request.ts
@@ -15,10 +15,18 @@ export type FetchedMetrics = CommitInfo & {
 
 const fetchJsonWithCache = async (url: string) => {
   const cacheKey = url;
-  const cachedData = await localForage.getItem(cacheKey).catch(() => {});
+  const cachedData = await localForage
+    .getItem<{ data: any; timestamp: number }>(cacheKey)
+    .catch(() => {});
 
   if (cachedData) {
-    return cachedData;
+    const { data, timestamp } = cachedData;
+    const currentTime = new Date().getTime();
+    const cacheExpiration = 12 * 60 * 60 * 1000; // 12 hour in milliseconds
+
+    if (currentTime - timestamp <= cacheExpiration) {
+      return data;
+    }
   }
 
   const data = await fetch(url)
@@ -26,7 +34,11 @@ const fetchJsonWithCache = async (url: string) => {
     .catch(() => {});
 
   if (data) {
-    await localForage.setItem(cacheKey, data);
+    const cacheData = {
+      data,
+      timestamp: new Date().getTime(),
+    };
+    await localForage.setItem(cacheKey, cacheData);
   }
 
   return data;
